e90e4743c0eaf9d454642366c13306c0e9e03fd0
Alaa Brahim <alaa.brahim@etudiant-fst.utm.tn>
2024-12-26 22:05:59 +0100

    configure Axon serialization with Jackson and update application properties

--- ventes/src/main/java/com/example/ventes/VentesApplication.java
+++ ventes/src/main/java/com/example/ventes/VentesApplication.java
@@ -2,18 +2,29 @@ package com.example.ventes;
 
 import org.axonframework.eventhandling.tokenstore.jpa.TokenEntry;
 import org.axonframework.modelling.saga.repository.jpa.SagaEntry;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Import;
 
 import com.example.coreapi.ventes.payment.PaymentInfo;
+import com.example.ventes.configuration.AxonConfig;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @EntityScan(basePackageClasses = { PaymentInfo.class, SagaEntry.class, TokenEntry.class })
 @SpringBootApplication
+@Import({ AxonConfig.class })
 public class VentesApplication {
 
 	public static void main(final String[] args) {
 		SpringApplication.run(VentesApplication.class, args);
+	}
+
+	@Autowired
+	public void configureSerializers(ObjectMapper objectMapper) {
+		objectMapper.activateDefaultTyping(objectMapper.getPolymorphicTypeValidator(),
+				ObjectMapper.DefaultTyping.JAVA_LANG_OBJECT);
 	}
 
 }
