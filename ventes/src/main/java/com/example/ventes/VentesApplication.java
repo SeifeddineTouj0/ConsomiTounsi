2967342343f5a6d87fedd4755529ec9351a53c09
Alaa Brahim <alaa.brahim@etudiant-fst.utm.tn>
2024-12-26 22:45:24 +0100

    remove AxonConfig class and update VentesApplication to eliminate unnecessary imports

--- ventes/src/main/java/com/example/ventes/VentesApplication.java
+++ ventes/src/main/java/com/example/ventes/VentesApplication.java
@@ -6,15 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Import;
-
 import com.example.coreapi.ventes.payment.PaymentInfo;
-import com.example.ventes.configuration.AxonConfig;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @EntityScan(basePackageClasses = { PaymentInfo.class, SagaEntry.class, TokenEntry.class })
 @SpringBootApplication
-@Import({ AxonConfig.class })
 public class VentesApplication {
 
 	public static void main(final String[] args) {
