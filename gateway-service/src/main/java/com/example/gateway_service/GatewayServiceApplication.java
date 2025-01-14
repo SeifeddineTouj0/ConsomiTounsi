6413c7d82cc305d51ef6d28ea6ea9b39095a0f79
Seifeddine Touj <seifeddine.touj@etudiant-fst.utm.tn>
2025-01-14 21:32:51 +0100

    Merge Remote Files

---
+++ gateway-service/src/main/java/com/example/gateway_service/GatewayServiceApplication.java
@@ -0,0 +1,1 @@
+﻿package com.example.gateway_service;import org.springframework.boot.SpringApplication;import org.springframework.boot.autoconfigure.SpringBootApplication;@SpringBootApplicationpublic class GatewayServiceApplication {public static void main(String[] args) {SpringApplication.run(GatewayServiceApplication.class, args);}}
