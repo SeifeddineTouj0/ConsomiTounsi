7afda7562456c08f08eb6f62657d063b20336a11
Fares <faresraboudi308@gmail.com>
2025-01-15 00:14:13 +0100

    add functionality and docker deploiment

--- core-api/src/main/java/com/example/coreapi/produits/commands/UpdateProductCommand.java
+++ core-api/src/main/java/com/example/coreapi/produits/commands/UpdateProductCommand.java
@@ -1,14 +1,18 @@
-package com.example.coreapi.produits.events;
+package com.example.coreapi.produits.commands;
 
 import com.example.coreapi.boutique.rayon.enumerations.RayonType;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.axonframework.modelling.command.TargetAggregateIdentifier;
 
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class ProductCreatedEvent {
+public class UpdateProductCommand {
+    @TargetAggregateIdentifier
     private String id;
     private String name;
     private String barCode;
