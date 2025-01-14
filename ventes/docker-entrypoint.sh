1cd94e4eda5b94383c7b2bbdaa48f252243042c6
Alaa Brahim <alaa.brahim@etudiant-fst.utm.tn>
2025-01-14 20:13:21 +0100

    fix: update config server URL in docker-entrypoint.sh for ventes microservice

--- ventes/docker-entrypoint.sh
+++ ventes/docker-entrypoint.sh
@@ -10,7 +10,7 @@ check_config_server() {
 
 # Function to check if the service config is available
 check_service_config() {
-    wget --spider -q http://config-server:8888/boutique-microservice/default 2>/dev/null
+    wget --spider -q http://config-server:8888/ventes-microservice/default 2>/dev/null
     return $?
 }
 
