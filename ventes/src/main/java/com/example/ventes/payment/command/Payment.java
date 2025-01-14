3dfbc257fee7847f1a7b5823c669fabf14c5c33b
Alaa Brahim <alaa.brahim@etudiant-fst.utm.tn>
2025-01-15 00:06:04 +0100

    feat: update Payment command to include stock update logic and improve stock validation

--- ventes/src/main/java/com/example/ventes/payment/command/Payment.java
+++ ventes/src/main/java/com/example/ventes/payment/command/Payment.java
@@ -1,15 +1,19 @@
 package com.example.ventes.payment.command;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 
 import org.axonframework.commandhandling.CommandHandler;
+import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.axonframework.eventsourcing.EventSourcingHandler;
 import org.axonframework.modelling.command.AggregateIdentifier;
 import org.axonframework.queryhandling.QueryGateway;
 import org.axonframework.spring.stereotype.Aggregate;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.example.coreapi.boutique.stock.commands.UpdateStockCommand;
 import com.example.coreapi.boutique.stock.queries.GetStockStatusQuery;
 import com.example.coreapi.boutique.stock.responses.StockStatusResponse;
 import com.example.coreapi.ventes.payment.*;
@@ -37,13 +41,20 @@ public class Payment {
     @Autowired
     private final QueryGateway queryGateway;
 
+    @Autowired
+    private final CommandGateway commandGateway;
+
     public Payment() {
         this.queryGateway = null; // Default constructor for Axon
+        this.commandGateway = null;
     }
 
     @CommandHandler
-    public Payment(CreatePaymentCommand command, QueryGateway queryGateway) {
+    public Payment(CreatePaymentCommand command, QueryGateway queryGateway, CommandGateway commandGateway) {
         this.queryGateway = queryGateway;
+        this.commandGateway = commandGateway;
+
+        Map<String, StockStatusResponse> productStockIdMap = new HashMap<>();
 
         for (PurchasedProduct produit : command.getProduitIds()) {
             if (produit.getQuantity() <= 0) {
@@ -57,6 +68,14 @@ public class Payment {
             if (stockStatusResponse.getQuantity() < produit.getQuantity()) {
                 throw new IllegalArgumentException("Quantite insuffisante");
             }
+            productStockIdMap.put(produit.getProductId(), stockStatusResponse);
+        }
+
+        // Update stock
+        for (PurchasedProduct produit : command.getProduitIds()) {
+            commandGateway.send(new UpdateStockCommand(productStockIdMap.get(produit.getProductId()).getStockId(),
+                    produit.getProductId(),
+                    productStockIdMap.get(produit.getProductId()).getQuantity() - produit.getQuantity()));
         }
 
         apply(new PaymentCreatedEvent(command.getPaymentId(), command.getTypePayment(), command.getMontant(),
